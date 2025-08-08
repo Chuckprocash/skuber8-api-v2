ee668b5ef5864ec86d6cf5476b18d06575fa66fd
chuckprocash <e.procashchakraborty@gmail.com>
2025-08-09 04:02:12 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return response('welcome to Railway.app', 200);
+    return ('welcome to Railway.app');
 });
